a582ab69f5d3b22cefe1de735a085b51ae332d86
Niraj More <moreniraj49@gmail.com>
2024-11-28 07:53:52 +0530

    added files for frontend

---
+++ src/components/common/check-auth.jsx
@@ -0,0 +1,71 @@
+import PropTypes from "prop-types";
+import { Navigate, useLocation } from "react-router-dom";
+
+function CheckAuth({ isAuthenticated, user, children }) {
+  const location = useLocation();
+
+  console.log(location.pathname, isAuthenticated);
+
+  if (location.pathname === "/") {
+    if (!isAuthenticated) {
+      return <Navigate to="/auth/login" />;
+    } else {
+      if (user?.role === "admin") {
+        return <Navigate to="/admin/dashboard" />;
+      } else {
+        return <Navigate to="/shop/home" />;
+      }
+    }
+  }
+
+  if (
+    !isAuthenticated &&
+    !(
+      location.pathname.includes("/login") ||
+      location.pathname.includes("/register")
+    )
+  ) {
+    return <Navigate to="/auth/login" />;
+  }
+
+  if (
+    isAuthenticated &&
+    (location.pathname.includes("/login") ||
+      location.pathname.includes("/register"))
+  ) {
+    if (user?.role === "admin") {
+      return <Navigate to="/admin/dashboard" />;
+    } else {
+      return <Navigate to="/shop/home" />;
+    }
+  }
+
+  if (
+    isAuthenticated &&
+    user?.role !== "admin" &&
+    location.pathname.includes("admin")
+  ) {
+    return <Navigate to="/unauth-page" />;
+  }
+
+  if (
+    isAuthenticated &&
+    user?.role === "admin" &&
+    location.pathname.includes("shop")
+  ) {
+    return <Navigate to="/admin/dashboard" />;
+  }
+
+  return <>{children}</>;
+}
+
+// Add Prop-Types validation
+CheckAuth.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired, // isAuthenticated should be a boolean
+  user: PropTypes.shape({
+    role: PropTypes.string, // user.role should be a string
+  }),
+  children: PropTypes.node, // children can be any renderable node
+};
+
+export default CheckAuth;
